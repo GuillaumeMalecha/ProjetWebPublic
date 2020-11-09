f2cbac0909aecc92dfd661abd34c1d53d2d55d93
Guillaume Malecha <guillaume.malecha@gmail.com>
2020-11-09 19:13:05 +0100

    update

--- assets/app.js
+++ assets/app.js
@@ -5,13 +5,36 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-import './css/plugins.css'
-import './css/nav.css'
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import './styles/colors.css';
+import 'bootstrap/scss/bootstrap.scss';
+import './css/plugins.css';
+import './css/nav.css';
+import './css/styles.css';
+import './css/colors.css';
+import './css/slick.scss';
+import './css/slick-theme.scss';
+
+//import './js/jquery.min';
+import $ from 'jquery';
+global.$ = global.jQuery = $
+
+
+
+import './js/circleMagic.min';
+import './js/popper.min';
+require('bootstrap');
+import './js/rangeslider';
+import './js/select2.min';
+import './js/aos';
+//import './js/owl.carousel.min';
+require('owl.carousel');
+import './js/jquery.magnific-popup.min';
+import './js/slick';
+import './js/slider-bg';
+import './js/lightbox';
+import './js/imagesloaded';
+import './js/isotope.min';
+import './js/coreNavigation';
+import './js/custom';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
